42bd49024cbcbdbe82c345fe2620f2bd61d365b1
beitaandbaozi <2280496040@qq.com>
2022-01-19 15:54:18 +0800

    vuex持久化-让在vuex中管理的状态数据同时存储在本地

--- erabbit-pc-vue-project/src/App.vue
+++ erabbit-pc-vue-project/src/App.vue
@@ -1,5 +1,5 @@
 <template>
 <!-- vue2.0需要根元素，vue3.0可以是代码片段 Fragment -->
-  <div>App</div>
-  <p>P</p>
+  <div>App {{$store.state.user.profile.account}}</div>
+  <button @click="$store.commit('user/setUser',{account:'beiTa'})">修改用户信息</button>
 </template>
--- erabbit-pc-vue-project/src/store/index.js
+++ erabbit-pc-vue-project/src/store/index.js
@@ -1,5 +1,12 @@
 import { createStore } from 'vuex'
 
+import cart from './modules/cart'
+import category from './modules/category'
+import user from './modules/user'
+
+// 引入持久化插件
+import createPersistedState from 'vuex-persistedstate'
+
 //  vue2.0 创建仓库 new Vuex.Store({})
 //  vue3.0 创建仓库 createStore({})
 export default createStore({
@@ -10,5 +17,19 @@ export default createStore({
   actions: {
   },
   modules: {
-  }
+    cart,
+    user,
+    category
+  },
+  // 配置插件
+  plugins: [
+    // 默认存储在localStorage上
+    // 修改state后触发才可以看到本地存储数据的的变化
+    createPersistedState({
+      // 本地存储的名字
+      key: 'beiTa-erabbit-client',
+      // 存储state中的那些数据，如果是模块下具体的数据需要加上模块名称，如user.token
+      paths: ['user', 'cart']
+    })
+  ]
 })
