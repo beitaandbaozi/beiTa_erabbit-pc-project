fa68831f9fc9f42da2c38535e14ab0879233a8bf
beitaandbaozi <2280496040@qq.com>
2022-01-20 16:20:01 +0800

    首页底部组件封装

--- erabbit-pc-vue-project/src/views/Layout.vue
+++ erabbit-pc-vue-project/src/views/Layout.vue
@@ -4,25 +4,30 @@
   <!-- 头部组件 -->
   <AppHeader></AppHeader>
   <!-- 内容容器 -->
-  <div class="main">
+  <div class="app-body">
     <!-- 二级路由 -->
     <router-view></router-view>
   </div>
   <!-- 底部组件 -->
-  <footer>底部组件</footer>
+  <AppFooter></AppFooter>
 </template>
 
 <script>
 import AppNavbar from "@/components/app-navbar";
 import AppHeader from "@/components/app-header";
+import AppFooter from "@/components/app-footer";
 export default {
   name: "Layout",
   components: {
     AppNavbar,
-    AppHeader
+    AppHeader,
+    AppFooter,
   },
 };
 </script>
 
 <style lang="less">
+.app-body {
+  min-height: 600px;
+}
 </style>
