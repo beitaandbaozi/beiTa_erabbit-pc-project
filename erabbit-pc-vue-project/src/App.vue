920a797ab332373dbcf6a4c68d315830312c4d80
beitaandbaozi <2280496040@qq.com>
2022-01-20 12:50:20 +0800

    路由初始部署

--- erabbit-pc-vue-project/src/App.vue
+++ erabbit-pc-vue-project/src/App.vue
@@ -1,18 +1,5 @@
 <template>
   <!-- vue2.0需要根元素，vue3.0可以是代码片段 Fragment -->
-  <div>App</div>
-  <button @click="fn">测试request工具函数</button>
+  <!-- 一级路由 -->
+  <router-view></router-view>
 </template>
-<script>
-import request from '@/utils/request'
-export default {
-  name: 'App',
-  setup () {
-    const fn = () => {
-      // 测试请求
-      request('/member/profile', 'get', { a: 10 })
-    }
-    return { fn }
-  }
-}
-</script>
---
+++ erabbit-pc-vue-project/src/views/home/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="container">
+    HomePages
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomePages',
+};
+</script>
+
+<style lang='less'>
+
+</style>
