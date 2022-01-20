c8920561cae872144551ec7e833ab69f7a23bd1b
beitaandbaozi <2280496040@qq.com>
2022-01-20 17:10:09 +0800

    提取头部分类导航组件，提供给头部，和将来的吸顶头部使用

--- erabbit-pc-vue-project/src/components/app-header.vue
+++ erabbit-pc-vue-project/src/components/app-header.vue
@@ -2,28 +2,25 @@
   <header class='app-header'>
     <div class="container">
       <!-- LOGO -->
-      <h1 class="logo"><RouterLink to="/">小兔鲜</RouterLink></h1>
-      <!-- 导航 -->
-      <ul class="navs">
-        <li class="home"><RouterLink to="/">首页</RouterLink></li>
-        <li><a href="#">美食</a></li>
-        <li><a href="#">餐厨</a></li>
-        <li><a href="#">艺术</a></li>
-        <li><a href="#">电器</a></li>
-        <li><a href="#">居家</a></li>
-        <li><a href="#">洗护</a></li>
-        <li><a href="#">孕婴</a></li>
-        <li><a href="#">服装</a></li>
-        <li><a href="#">杂货</a></li>
-      </ul>
+      <h1 class="logo">
+        <RouterLink to="/">小兔鲜</RouterLink>
+      </h1>
+      <!-- 头部导航组件 -->
+      <AppHeaderNav></AppHeaderNav>
       <!-- 搜索 -->
       <div class="search">
         <i class="iconfont icon-search"></i>
-        <input type="text" placeholder="搜一搜">
+        <input
+          type="text"
+          placeholder="搜一搜"
+        >
       </div>
       <!-- 购物车 -->
       <div class="cart">
-        <a class="curr" href="#">
+        <a
+          class="curr"
+          href="#"
+        >
           <i class="iconfont icon-cart"></i><em>2</em>
         </a>
       </div>
@@ -32,9 +29,14 @@
 </template>
 
 <script>
+// 引入头像分类导航组件
+import AppHeaderNav from "@/components/app-header-nav";
 export default {
-  name: 'AppHeader'
-}
+  name: "AppHeader",
+  components: {
+    AppHeaderNav,
+  },
+};
 </script>
 
 <style scoped lang='less'>
@@ -52,29 +54,6 @@ export default {
       width: 100%;
       text-indent: -9999px;
       background: url(../assets/images/logo.png) no-repeat center 18px / contain;
-    }
-  }
-  .navs {
-    width: 820px;
-    display: flex;
-    justify-content: space-around;
-    padding-left: 40px;
-    li {
-      margin-right: 40px;
-      width: 38px;
-      text-align: center;
-      a {
-        font-size: 16px;
-        line-height: 32px;
-        height: 32px;
-        display: inline-block;
-      }
-      &:hover {
-        a {
-          color: @xtxColor;
-          border-bottom: 1px solid @xtxColor;
-        }
-      }
     }
   }
   .search {
@@ -101,7 +80,7 @@ export default {
       text-align: center;
       position: relative;
       display: block;
-      .icon-cart{
+      .icon-cart {
         font-size: 22px;
       }
       em {
