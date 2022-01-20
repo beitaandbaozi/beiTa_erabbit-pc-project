ad65d62e9beec09f0b0f5aa60720f56447415d61
beitaandbaozi <2280496040@qq.com>
2022-01-20 14:31:30 +0800

    样式的重置与公用

--- erabbit-pc-vue-project/src/main.js
+++ erabbit-pc-vue-project/src/main.js
@@ -2,5 +2,9 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+// 引入重置样式包
+import 'normalize.css'
+// 引入自己项目的重置样式和公用样式
+import '@/assets/styles/common.less'
 
 createApp(App).use(store).use(router).mount('#app')
