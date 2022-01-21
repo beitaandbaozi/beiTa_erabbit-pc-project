0cba8fd99ddae7054c2d558d85946e2c52c152a1
beitaandbaozi <2280496040@qq.com>
2022-01-21 21:52:47 +0800

    首页主体左侧分类中结构渲染完成

--- erabbit-pc-vue-project/src/views/home/index.vue
+++ erabbit-pc-vue-project/src/views/home/index.vue
@@ -1,18 +1,23 @@
 <template>
   <div class="container">
-    HomePages
+    <!-- 首页入口（分类+广告） -->
+    <div class="home-entry">
+      <!-- 左侧分类 -->
+      <HomeCategory></HomeCategory>
+      <!-- 轮播图 -->
+    </div>
   </div>
 </template>
 
 <script>
+import HomeCategory from "./components/home-category.vue"
 export default {
   name: "HomePages",
+  components: {
+    HomeCategory
+  }
 };
 </script>
 
 <style scoped lang='less'>
-.container {
-  .hoverShadow();
-  color:@xtxColor
-}
 </style>
