c72eb17cb8f0d070d86f50d139cf703c2cc9e0f5
beitaandbaozi <2280496040@qq.com>
2022-01-22 00:31:59 +0800

    首页左侧分类（品牌处理完成）

--- erabbit-pc-vue-project/src/views/home/components/home-category.vue
+++ erabbit-pc-vue-project/src/views/home/components/home-category.vue
@@ -1,10 +1,11 @@
 <template>
-  <div class='home-category'>
-    <ul class="menu">
+  <div class='home-category' @mouseleave="categoryId = null">
+    <ul class="menu" >
       <li
         v-for="item in menuList"
         :key="item.id"
         @mouseenter="categoryId = item.id"
+        :class="{active:categoryId===item.id}"
       >
         <RouterLink :to="`/category/${item.id}`">{{item.name}}</RouterLink>
         <!-- 二级类目 -->
@@ -19,7 +20,8 @@
     </ul>
     <!-- 弹层 -->
     <div class="layer">
-      <h4>分类推荐 <small>根据您的购买或浏览记录推荐</small></h4>
+      <h4>{{currCategory&&currCategory.id==='brand'?'品牌':'分类'}}推荐 <small>根据您的购买或浏览记录推荐</small></h4>
+      <!-- 商品 -->
       <ul v-if="currCategory && currCategory.goods">
         <li
           v-for="item in currCategory.goods"
@@ -38,6 +40,26 @@
           </RouterLink>
         </li>
       </ul>
+      <!-- 品牌 -->
+      <ul v-if="currCategory && currCategory.brands">
+        <li
+          class="brand"
+          v-for="brand in currCategory.brands"
+          :key="brand.id"
+        >
+          <RouterLink to="/">
+            <img
+              :src="brand.picture"
+              alt=""
+            >
+            <div class="info">
+              <p class="place"><i class="iconfont icon-dingwei"></i>{{brand.place}}</p>
+              <p class="name ellipsis">{{brand.name}}</p>
+              <p class="desc ellipsis-2">{{brand.desc}}</p>
+            </div>
+          </RouterLink>
+        </li>
+      </ul>
     </div>
   </div>
 </template>
@@ -45,6 +67,7 @@
 <script>
 import { computed, reactive, ref } from "vue";
 import { useStore } from "vuex";
+import { findBrand } from "@/api/home";
 export default {
   name: "HomeCategory",
   setup () {
@@ -54,6 +77,8 @@ export default {
       id: "brand",
       name: "品牌",
       children: [{ id: "brand-children", name: "品牌推荐" }],
+      // 品牌列表
+      brands: [],
     });
     const menuList = computed(() => {
       // 得到9个分类下每一个一级分类下的子分类只有两个
@@ -74,6 +99,12 @@ export default {
       // 记得ref数据取值时用 .value
       return menuList.value.find((item) => item.id === categoryId.value);
     });
+
+    // 获取品牌数据 (setup不支持async await 方式)
+    findBrand().then((data) => {
+      brand.brands = data.result;
+    });
+
     return { menuList, categoryId, currCategory };
   },
 };
@@ -91,7 +122,7 @@ export default {
       padding-left: 40px;
       height: 50px;
       line-height: 50px;
-      &:hover {
+      &:hover,&.active {
         background: @xtxColor;
       }
       a {
@@ -170,6 +201,26 @@ export default {
           }
         }
       }
+      // 品牌样式
+        li.brand {
+          height: 180px;
+          a {
+            align-items: flex-start;
+            img {
+              width: 120px;
+              height: 160px;
+            }
+            .info {
+              p {
+                margin-top: 8px;
+              }
+              .place {
+                color: #999;
+              }
+            }
+          }
+        }
+
     }
   }
   &:hover {
