03c643aba3167e0e487e71fcb437fd136575390a
beitaandbaozi <2280496040@qq.com>
2022-01-21 23:56:25 +0800

    首页主体左侧分类弹层展示渲染完成（“品牌未处理”）

--- erabbit-pc-vue-project/src/views/home/components/home-category.vue
+++ erabbit-pc-vue-project/src/views/home/components/home-category.vue
@@ -4,45 +4,78 @@
       <li
         v-for="item in menuList"
         :key="item.id"
+        @mouseenter="categoryId = item.id"
       >
         <RouterLink :to="`/category/${item.id}`">{{item.name}}</RouterLink>
         <!-- 二级类目 -->
         <template v-if="item.children">
-          <RouterLink v-for="sub in item.children" :key="sub.id" :to="`/category/sub/${sub.id}`">{{sub.name}}</RouterLink>
+          <RouterLink
+            v-for="sub in item.children"
+            :key="sub.id"
+            :to="`/category/sub/${sub.id}`"
+          >{{sub.name}}</RouterLink>
         </template>
       </li>
     </ul>
+    <!-- 弹层 -->
+    <div class="layer">
+      <h4>分类推荐 <small>根据您的购买或浏览记录推荐</small></h4>
+      <ul v-if="currCategory && currCategory.goods">
+        <li
+          v-for="item in currCategory.goods"
+          :key="item.id"
+        >
+          <RouterLink to="/">
+            <img
+              :src="item.picture"
+              alt=""
+            >
+            <div class="info">
+              <p class="name ellipsis-2">{{item.name}}</p>
+              <p class="desc ellipsis">{{item.desc}}</p>
+              <p class="price"><i>¥</i>{{item.price}}</p>
+            </div>
+          </RouterLink>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
-import { computed, reactive } from 'vue';
-import { useStore } from 'vuex';
+import { computed, reactive, ref } from "vue";
+import { useStore } from "vuex";
 export default {
   name: "HomeCategory",
   setup () {
     const store = useStore();
     // 最终数据 = 9个分类 + 1个品牌
     const brand = reactive({
-      id: 'brand',
-      name: '品牌',
-      children: [{ id: 'brand-children', name: '品牌推荐' }]
-    })
+      id: "brand",
+      name: "品牌",
+      children: [{ id: "brand-children", name: "品牌推荐" }],
+    });
     const menuList = computed(() => {
       // 得到9个分类下每一个一级分类下的子分类只有两个
-      const list = store.state.category.list.map(item => {
+      const list = store.state.category.list.map((item) => {
         return {
           id: item.id,
           name: item.name,
           children: item.children && item.children.slice(0, 2),
-          goods: item.goods
-        }
-      })
-      list.push(brand)
-      return list
-    })
-    return { menuList }
-  }
+          goods: item.goods,
+        };
+      });
+      list.push(brand);
+      return list;
+    });
+    // 得到弹层的推荐商品数据
+    const categoryId = ref(null);
+    const currCategory = computed(() => {
+      // 记得ref数据取值时用 .value
+      return menuList.value.find((item) => item.id === categoryId.value);
+    });
+    return { menuList, categoryId, currCategory };
+  },
 };
 </script>
 
@@ -68,6 +101,80 @@ export default {
           font-size: 16px;
         }
       }
+    }
+  }
+  // 弹层样式
+  .layer {
+    width: 990px;
+    height: 500px;
+    background: rgba(255, 255, 255, 0.8);
+    position: absolute;
+    left: 250px;
+    top: 0;
+    display: none;
+    padding: 0 15px;
+    h4 {
+      font-size: 20px;
+      font-weight: normal;
+      line-height: 80px;
+      small {
+        font-size: 16px;
+        color: #666;
+      }
+    }
+    ul {
+      display: flex;
+      flex-wrap: wrap;
+      li {
+        width: 310px;
+        height: 120px;
+        margin-right: 15px;
+        margin-bottom: 15px;
+        border: 1px solid #eee;
+        border-radius: 4px;
+        background: #fff;
+        &:nth-child(3n) {
+          margin-right: 0;
+        }
+        a {
+          display: flex;
+          width: 100%;
+          height: 100%;
+          align-items: center;
+          padding: 10px;
+          &:hover {
+            background: #e3f9f4;
+          }
+          img {
+            width: 95px;
+            height: 95px;
+          }
+          .info {
+            padding-left: 10px;
+            line-height: 24px;
+            width: 190px;
+            .name {
+              font-size: 16px;
+              color: #666;
+            }
+            .desc {
+              color: #999;
+            }
+            .price {
+              font-size: 22px;
+              color: @priceColor;
+              i {
+                font-size: 16px;
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+  &:hover {
+    .layer {
+      display: block;
     }
   }
 }
