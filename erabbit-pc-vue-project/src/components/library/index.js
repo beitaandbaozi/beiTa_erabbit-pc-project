aedef2ec1cb21a8a403944dea23acf463cb3c2ee
beitaandbaozi <2280496040@qq.com>
2022-01-22 15:03:39 +0800

    首页主体轮播图基本布局完成

--- erabbit-pc-vue-project/src/components/library/index.js
+++ erabbit-pc-vue-project/src/components/library/index.js
@@ -2,11 +2,13 @@
 // vue2.0插件写法要素：导出一个对象，有install函数，默认传入了vue构造函数，vue基础上扩展
 // vue3.0插件写法要素：导出一个对象，有install函数，默认传入了App,vue基础上扩展
 import XtxSkeleton from './xtx-skeleton.vue'
+import XtxCarousel from './xtx-carousel.vue'
 
 export default {
   install (app) {
     // 在app上进行扩展，app提供 component directive
     // 挂载原型： app.config.globalProperties方式
     app.component(XtxSkeleton.name, XtxSkeleton)
+    app.component(XtxCarousel.name, XtxCarousel)
   }
 }
