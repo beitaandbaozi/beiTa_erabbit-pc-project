0d7f73e26f16e33c572ebd199f9de6a03ad3b798
beitaandbaozi <2280496040@qq.com>
2022-01-24 14:40:26 +0800

    使用render函数封装面包屑（高级）

--- erabbit-pc-vue-project/src/components/library/xtx-bread-item.vue
+++ erabbit-pc-vue-project/src/components/library/xtx-bread-item.vue
@@ -6,7 +6,6 @@
     ><slot /></RouterLink>
     <span v-else><slot /></span>
   </div>
-  <i class="iconfont icon-angle-right"></i>
 </template>
 <script>
 export default {
--- erabbit-pc-vue-project/src/components/library/xtx-bread.vue
+++ erabbit-pc-vue-project/src/components/library/xtx-bread.vue
@@ -1,12 +1,28 @@
-<template>
-  <div class='xtx-bread'>
-    <slot />
-  </div>
-</template>
-
 <script>
+import { h } from "vue";
 export default {
   name: "XtxBread",
+  render () {
+    // 1. template 标签去除， 单文件组件
+    // 2. 返回值就是组件内容
+    // 3. vue2.0的h函数传参进来的，vue3.0的h函数导入进来
+    // 4. h 第一个参数  标签名字  第二个参数  标签属性对象  第三个参数  子节点
+    // 需求（封装面包屑）
+    // 1. 创建xtx-bread父容器
+    // 2. 获取默认插槽内容
+    // 3. 去除xtx-bread-item组件的i标签，应该由render来创建
+    // 4. 遍历插槽中的item，得到一个动态创建的节点，最后一个item不加i标签
+    // 5. 把动态创建的节点渲染在xtx-bread标签中
+    const items = this.$slots.default();
+    const dynamicItems = [];
+    items.forEach((item, i) => {
+      dynamicItems.push(item);
+      if (i < items.length - 1) {
+        dynamicItems.push(h("i", { class: "iconfont icon-angle-right" }));
+      }
+    });
+    return h("div", { class: "xtx-bread" }, dynamicItems);
+  },
 };
 </script>
 
