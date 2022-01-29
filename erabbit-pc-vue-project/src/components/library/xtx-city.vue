64beca2c59583e3f80ba198efe4fd4023c31f321
beitaandbaozi <2280496040@qq.com>
2022-01-29 22:33:00 +0800

    城市选择逻辑完成

--- erabbit-pc-vue-project/src/components/library/xtx-city.vue
+++ erabbit-pc-vue-project/src/components/library/xtx-city.vue
@@ -8,8 +8,14 @@
       @click="toggle"
       :class="{active:visible}"
     >
-      <span class="placeholder">请选择配送地址</span>
-      <span class="value"></span>
+      <span
+        class="placeholder"
+        v-if="!fullLocation"
+      >请选择配送地址</span>
+      <span
+        class="value"
+        v-else
+      >{{fullLocation}}</span>
       <i class="iconfont icon-angle-down"></i>
     </div>
     <div
@@ -25,18 +31,25 @@
           class="ellipsis"
           v-for="item in currList"
           :key="item.code"
+          @click="changeItem(item)"
         >{{item.name}}</span>
       </template>
     </div>
   </div>
 </template>
 <script>
-import { ref, computed } from "vue";
+import { ref, computed, reactive } from "vue";
 import { onClickOutside } from "@vueuse/core";
 import axios from "axios";
 export default {
   name: "XtxCity",
-  setup () {
+  props: {
+    fullLocation: {
+      type: String,
+      default: "",
+    },
+  },
+  setup (props, { emit }) {
     // 显示和隐藏数据
     const visible = ref(false);
     // 城市数据
@@ -52,6 +65,10 @@ export default {
         allCityData.value = data;
         loading.value = false;
       });
+      // 清空之前选中地址
+      for (const key in changeResult) {
+        changeResult[key] = "";
+      }
     };
     const close = () => {
       visible.value = false;
@@ -72,12 +89,52 @@ export default {
     // 实现计算属性： 获取当前显示的地区数组
     const currList = computed(() => {
       // 默认显示省一级
-      const list = allCityData.value;
+      let list = allCityData.value;
       // 可能：市一级
+      if (changeResult.provinceCode && changeResult.provinceName) {
+        list = list.find((p) => p.code === changeResult.provinceCode).areaList;
+      }
       // 可能：县地区一级
+      if (changeResult.cityCode && changeResult.cityName) {
+        list = list.find((c) => c.code === changeResult.cityCode).areaList;
+      }
       return list;
     });
-    return { visible, toggle, target, loading, currList };
+
+    // 定义选择的 省市区 数据
+    const changeResult = reactive({
+      // 省
+      provinceCode: "",
+      provinceName: "",
+      // 市
+      cityCode: "",
+      cityName: "",
+      // 地区
+      countyCode: "",
+      countyName: "",
+      fullLocation: "",
+    });
+    // 当点击地区时,记录
+    const changeItem = (item) => {
+      if (item.level === 0) {
+        // 省
+        changeResult.provinceCode = item.code;
+        changeResult.provinceName = item.name;
+      } else if (item.level === 1) {
+        // 市
+        changeResult.cityCode = item.code;
+        changeResult.cityName = item.name;
+      } else if (item.level === 2) {
+        // 地区
+        changeResult.countyCode = item.code;
+        changeResult.countyName = item.name;
+        // 这是最后一级，选完了，关闭对话框，通知父组件数据更新
+        changeResult.fullLocation = `${changeResult.provinceName} ${changeResult.cityName} ${changeResult.countyName}`;
+        close();
+        emit("change", changeResult);
+      }
+    };
+    return { visible, toggle, target, loading, currList, changeItem };
   },
 };
 // 获取省市区数据函数
