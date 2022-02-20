cd9c7774e3892f2927e7c47cc912ed586b2c7c77
beitaandbaozi <2280496040@qq.com>
2022-02-20 15:35:02 +0800

    登录组件配置

--- erabbit-pc-vue-project/src/router/index.js
+++ erabbit-pc-vue-project/src/router/index.js
@@ -5,6 +5,7 @@ const Home = () => import('@/views/home')
 const TopCategory = () => import('@/views/category/index')
 const SubCategory = () => import('@/views/category/sub')
 const Goods = () => import('@/views/goods/index')
+const Login = () => import('@/views/login/index')
 // 路由规则
 const routes = [
   // 一级路由布局容器
@@ -17,7 +18,8 @@ const routes = [
       { path: '/category/sub/:id', component: SubCategory },
       { path: '/product/:id', component: Goods },
     ]
-  }
+  },
+  { path: '/login', component: Login }
 ]
 
 // vue2.0 new VueRouter({}) 创建路由实例
@@ -27,7 +29,7 @@ const router = createRouter({
   history: createWebHashHistory(),
   routes,
   // 每次切换路由的时候切换到页面的顶端
-  scrollBehavior () {
+  scrollBehaviorn () {
     // vue2.0 x y 控制
     // vue3.0 left top 控制
     return { left: 0, top: 0 }
