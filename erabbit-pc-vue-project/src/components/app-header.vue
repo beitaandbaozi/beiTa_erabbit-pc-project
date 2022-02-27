27d9af2373ee61687ba8bffa921ecd1254a77ad2
beitaandbaozi <2280496040@qq.com>
2022-02-27 16:56:51 +0800

    购物车模块-头部购物车基础布局数据渲染完成

--- erabbit-pc-vue-project/src/components/app-header.vue
+++ erabbit-pc-vue-project/src/components/app-header.vue
@@ -16,14 +16,8 @@
         >
       </div>
       <!-- 购物车 -->
-      <div class="cart">
-        <a
-          class="curr"
-          href="#"
-        >
-          <i class="iconfont icon-cart"></i><em>2</em>
-        </a>
-      </div>
+      <!-- 使用头部购物车组件 -->
+      <AppHeaderCart></AppHeaderCart>
     </div>
   </header>
 </template>
@@ -31,10 +25,12 @@
 <script>
 // 引入头像分类导航组件
 import AppHeaderNav from "@/components/app-header-nav";
+import AppHeaderCart from "@/components/app-header-cart";
 export default {
   name: "AppHeader",
   components: {
     AppHeaderNav,
+    AppHeaderCart
   },
 };
 </script>
@@ -70,32 +66,6 @@ export default {
       width: 140px;
       padding-left: 5px;
       color: #666;
-    }
-  }
-  .cart {
-    width: 50px;
-    .curr {
-      height: 32px;
-      line-height: 32px;
-      text-align: center;
-      position: relative;
-      display: block;
-      .icon-cart {
-        font-size: 22px;
-      }
-      em {
-        font-style: normal;
-        position: absolute;
-        right: 0;
-        top: 0;
-        padding: 1px 6px;
-        line-height: 1;
-        background: @helpColor;
-        color: #fff;
-        font-size: 12px;
-        border-radius: 10px;
-        font-family: Arial;
-      }
     }
   }
 }
