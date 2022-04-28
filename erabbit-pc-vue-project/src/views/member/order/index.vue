3b73896ac40f2b2bcd904e9a2986df6e267f7f49
beitaandbaozi <2280496040@qq.com>
2022-04-29 00:03:54 +0800

    feat:将订单商品提取成组件并渲染

--- erabbit-pc-vue-project/src/views/member/order/index.vue
+++ erabbit-pc-vue-project/src/views/member/order/index.vue
@@ -6,23 +6,45 @@
         :key="item.name"
         :label="item.label"
         :name="item.name"
-        >{{ item.label }}</XtxTabsPanel
-      >
+      ></XtxTabsPanel>
     </XtxTabs>
+    <!-- 订单列表 -->
+    <div class="order-list">
+      <OrderItem
+        v-for="item in orderList"
+        :key="item.id"
+        :order="item"
+      ></OrderItem>
+    </div>
+    <!-- 分页组件 -->
+    <XtxPagination></XtxPagination>
   </div>
 </template>
 
 <script>
-import { ref } from "vue";
+import { reactive, ref } from "vue";
 import { orderStatus } from "@/api/constants";
+import OrderItem from "./components/order-item";
+import { findOrderList } from "@/api/order";
 export default {
   name: "OrderMember",
+  components: { OrderItem },
   setup () {
     const activeName = ref("all");
     const tabClick = (tab) => {
       console.log(tab);
     };
-    return { activeName, tabClick, orderStatus };
+    const orderList = ref([]);
+    const reqParams = reactive({
+      page: 1,
+      pageSize: 5,
+      orderState: 0
+    });
+    // 查询订单
+    findOrderList(reqParams).then((data) => {
+      orderList.value = data.result.items;
+    });
+    return { activeName, tabClick, orderStatus, orderList };
   }
 };
 </script>
@@ -31,5 +53,8 @@ export default {
 .member-order {
   height: 100%;
   background-color: #fff;
+  .order-list {
+    padding: 20px;
+  }
 }
 </style>
