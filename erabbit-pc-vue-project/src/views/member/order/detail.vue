55c7d5c133a3d74e097750bffd0db603fe146408
beitaandbaozi <2280496040@qq.com>
2022-04-29 21:33:41 +0800

    feat:订单详情-头部展示

--- erabbit-pc-vue-project/src/views/member/order/detail.vue
+++ erabbit-pc-vue-project/src/views/member/order/detail.vue
@@ -1,12 +1,36 @@
 <template>
-  <div>MemberDetail</div>
+  <div class="member-order-detail" v-if="order">
+    <!-- 头部 -->
+    <DetailAction :order="order"></DetailAction>
+    <!-- 进度 -->
+    <!-- 物流 -->
+    <!-- 信息 -->
+  </div>
 </template>
 
 <script>
+import { ref } from "vue";
+import DetailAction from "./components/detail-action.vue";
+import { findOrder } from "@/api/order";
+import { useRoute } from "vue-router";
 export default {
   name: "MemberDetail",
-  setup () {}
+  components: { DetailAction },
+  setup () {
+    const order = ref(null);
+    const route = useRoute();
+    // 获取当前订单数据
+    findOrder(route.params.id).then((data) => {
+      order.value = data.result;
+    });
+    return { order };
+  }
 };
 </script>
 
-<style lang="less" scoped></style>
+<style lang="less" scoped>
+.member-order-detail {
+  background-color: #fff;
+  height: 100%;
+}
+</style>
