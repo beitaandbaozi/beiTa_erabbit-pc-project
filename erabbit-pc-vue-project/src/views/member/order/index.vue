d380489bea1cee57cae8fb0eb2ce48c7c35c915c
beitaandbaozi <2280496040@qq.com>
2022-04-29 10:27:46 +0800

    feat:完成tab切换处理（watch监听类型变化）

--- erabbit-pc-vue-project/src/views/member/order/index.vue
+++ erabbit-pc-vue-project/src/views/member/order/index.vue
@@ -22,7 +22,7 @@
 </template>
 
 <script>
-import { reactive, ref } from "vue";
+import { reactive, ref, watch } from "vue";
 import { orderStatus } from "@/api/constants";
 import OrderItem from "./components/order-item";
 import { findOrderList } from "@/api/order";
@@ -31,19 +31,34 @@ export default {
   components: { OrderItem },
   setup () {
     const activeName = ref("all");
-    const tabClick = (tab) => {
-      console.log(tab);
-    };
     const orderList = ref([]);
     const reqParams = reactive({
       page: 1,
       pageSize: 5,
       orderState: 0
     });
-    // 查询订单
-    findOrderList(reqParams).then((data) => {
-      orderList.value = data.result.items;
-    });
+    // 条件渲染
+    const tabClick = ({ index }) => {
+      // 1.初始化页数
+      reqParams.page = 1;
+      // 2.更换类别
+      reqParams.orderState = index;
+      // 3.重新加载
+    };
+    // 根据点击tab时 orderState类型发生变化，就重新加载
+    watch(
+      reqParams,
+      () => {
+        findOrderList(reqParams).then((data) => {
+          orderList.value = data.result.items;
+        });
+      },
+      { immediate: true }
+    );
+    // // 查询订单
+    // findOrderList(reqParams).then((data) => {
+    //   orderList.value = data.result.items;
+    // });
     return { activeName, tabClick, orderStatus, orderList };
   }
 };
